fcdf076e30d4b8cd9b06f797632f61f2908b95e9
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-05-18 13:06:40 -0300

    [INFRA]: configure middlewares and body-parser

--- backend/src/main/config/app.ts
+++ backend/src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import middlewares from './middlewares';
 import routes from './routes';
 
 const app = express();
 
 app.use(routes);
+app.use(middlewares);
 
 export default app;
