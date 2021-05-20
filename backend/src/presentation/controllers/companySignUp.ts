10c9dd3ee008669e64123460a878d0b68e40575b
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-05-20 10:47:27 -0300

    [INFRA]: add server error response

--- backend/src/presentation/controllers/companySignUp.ts
+++ backend/src/presentation/controllers/companySignUp.ts
@@ -1,6 +1,7 @@
 import { CompanySignUpParams } from '@domain/entities/company';
+import { BaseError } from '@domain/errors';
 import { CompanySignUp } from '@domain/useCases';
-import { appError, noContent } from '@presentation/helpers/httpHelper';
+import { appError, noContent, serverError } from '@presentation/helpers/httpHelper';
 import { Controller, HttpResponse } from '@presentation/protocols';
 import { Validation } from '@validation/protocols/validation';
 
@@ -13,7 +14,8 @@ export class CompanySignUpController implements Controller {
       this.companySignUpValidation.validate(request);
       await this.companySignUp.execute(request);
     } catch (error) {
-      return appError(error.message, error.statusCode);
+      if (error instanceof BaseError) return appError(error.message, error.statusCode);
+      return serverError();
     }
     return noContent();
   }
--- backend/src/presentation/helpers/httpHelper.ts
+++ backend/src/presentation/helpers/httpHelper.ts
@@ -5,6 +5,11 @@ export const appError = (MessageError: string, statusCode = 400): HttpResponse =
   body: { error: MessageError },
 });
 
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: { error: 'Internal server error' },
+});
+
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data,
