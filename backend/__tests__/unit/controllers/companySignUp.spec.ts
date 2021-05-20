37363bf2bb37aed86709f1a8897104310b283856
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-05-20 10:14:17 -0300

    [REFACTOR]:  change Test companySignUp request data.

--- backend/__tests__/unit/controllers/companySignUp.spec.ts
+++ backend/__tests__/unit/controllers/companySignUp.spec.ts
@@ -25,13 +25,12 @@ describe('Company SignUp', () => {
 
     const data = {
       owner_company_name: 'any',
-      owner_company_phonenumber: 0,
       owner_company_cpf: 0,
       owner_company_role: 'any',
       company_cnpj: 0,
       company_name: 'any',
-      company_email: 'any',
-      company_phonenumber: 0,
+      email: 'any',
+      phonenumber: 0,
       company_area: 'any',
     };
     const { body, statusCode } = await sut.handle(data);
@@ -43,13 +42,12 @@ describe('Company SignUp', () => {
     const spyService = jest.spyOn(companySignUpservice, 'execute');
     const data = {
       owner_company_name: 'any',
-      owner_company_phonenumber: 0,
       owner_company_cpf: 0,
       owner_company_role: 'any',
       company_cnpj: 0,
       company_name: 'any',
-      company_email: 'any',
-      company_phonenumber: 0,
+      email: 'any',
+      phonenumber: 0,
       company_area: 'any',
     };
     await sut.handle(data);
