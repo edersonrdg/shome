76ecae4a82090dd878b970fde7bf36873727c1bc
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-05-21 11:37:02 -0300

    [TEST]: ensure companySignUp service returns error if owner cpf is already used

--- backend/__tests__/unit/services/companySignUp.spec.ts
+++ backend/__tests__/unit/services/companySignUp.spec.ts
@@ -1,11 +1,16 @@
 import { CompanyRepository } from '@application/protocols/db';
 import { CompanySignUpDTO } from '@application/protocols/dtos';
 import { CompanySignUpService } from '@application/services';
+import { BaseError } from '@domain/errors';
 
 const makeCompanyRepository = () => {
   class CompanyRepositoryStub implements CompanyRepository {
     async create(data: CompanySignUpDTO): Promise<void> {
       return new Promise((resolve) => resolve());
+    }
+
+    async findByOwnerCpf(cpf: number): Promise<boolean> {
+      return new Promise((resolve) => resolve(false));
     }
   }
   return new CompanyRepositoryStub();
@@ -34,6 +39,27 @@ describe('CompanySignUp Service', () => {
 
     const response = await sut.execute(data);
     expect(response).toBe(undefined);
+  });
+  it('should returns error if owner cpf is already used', async () => {
+    const { sut, companyRepository } = makeSut();
+    jest.spyOn(companyRepository, 'findByOwnerCpf').mockImplementation((cpf: number) => new Promise((resolve) => resolve(true)));
+
+    const data = {
+      owner_company_name: 'any',
+      owner_company_cpf: 0,
+      owner_company_role: 'any',
+      company_cnpj: 0,
+      company_name: 'any',
+      email: 'any',
+      phonenumber: 0,
+      company_area: 'any',
+    };
+    try {
+      await sut.execute(data);
+    } catch (error) {
+      expect(error).toEqual(new BaseError('Owner cpf is already using on another company'));
+      expect(error.statusCode).toBe(400);
+    }
   });
   it('should call CompanyRepository with valid data', async () => {
     const { sut, companyRepository } = makeSut();
