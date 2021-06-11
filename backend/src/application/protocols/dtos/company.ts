59b6433b2801ad6c3a220d133cf37c4ad35efc34
Ederson S <edersonrodrigo31@gmail.com>
2021-06-11 19:35:57 -0300

    [REFACTOR]: change companySignUpDto

--- backend/src/application/protocols/dtos/company.ts
+++ backend/src/application/protocols/dtos/company.ts
@@ -1,3 +1,10 @@
-import { CompanySignUpParams } from '@domain/entities';
-
-export type CompanySignUpDTO = CompanySignUpParams
+export type CompanySignUpDTO = {
+  owner_company_name: string
+  owner_company_cpf: number
+  owner_company_role: string
+  company_cnpj: number
+  company_name: string
+  company_area: string
+  phonenumber: number
+  email: string
+}
