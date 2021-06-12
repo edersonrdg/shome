960031f972dfd9efc15aec48460807f1dfb14f10
Ederson S <edersonrodrigo31@gmail.com>
2021-06-11 22:00:32 -0300

    [TEST]: ensure company list controller calls service

--- backend/__tests__/unit/controllers/companyList.spec.ts
+++ backend/__tests__/unit/controllers/companyList.spec.ts
@@ -1,8 +1,10 @@
 import { CompanyListController } from '@presentation/controllers/companyList';
+import { CompanyListStub } from '../moks/makeService';
 
 const makeSut = () => {
-  const sut = new CompanyListController();
-  return { sut };
+  const service = new CompanyListStub();
+  const sut = new CompanyListController(service);
+  return { sut, service };
 };
 
 describe('Company list', () => {
@@ -11,5 +13,12 @@ describe('Company list', () => {
 
     const response = await sut.handle();
     expect(response.statusCode).toBe(200);
+  });
+  it('Should calls company list service', async () => {
+    const { sut, service } = makeSut();
+    const spyService = jest.spyOn(service, 'execute');
+
+    await sut.handle();
+    expect(spyService).toBeCalled();
   });
 });
--- backend/__tests__/unit/moks/makeService.ts
+++ backend/__tests__/unit/moks/makeService.ts
@@ -1,7 +1,15 @@
+import { Company } from '@domain/entities';
 import { CompanySignUp, CompanySignUpParams } from '@domain/useCases';
+import { CompanyList } from '@domain/useCases/companyList';
 
 export class CompanySignUpStub implements CompanySignUp {
   async execute(account: CompanySignUpParams): Promise<void> {
     return new Promise((resolve) => resolve());
+  }
+}
+
+export class CompanyListStub implements CompanyList {
+  async execute(): Promise<Company[]> {
+    return new Promise((resolve) => resolve([]));
   }
 }
