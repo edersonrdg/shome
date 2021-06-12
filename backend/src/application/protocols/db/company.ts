5031ee6044b2e4f0de67b9879d9fc87674241c22
Ederson S <edersonrodrigo31@gmail.com>
2021-06-11 22:33:10 -0300

    [REFACTOR]: add getAll function to company repository

--- backend/src/application/protocols/db/company.ts
+++ backend/src/application/protocols/db/company.ts
@@ -1,7 +1,10 @@
+import { Company } from '@domain/entities';
 import { CompanySignUpParams } from '@domain/useCases';
 
 export interface CompanyRepository {
   create: (data: CompanySignUpParams) => Promise<void>
 
   findByOwnerCpf: (ownercpf: number) => Promise<boolean>
+
+  getAll: () => Promise<Company[]>
 }
--- backend/src/infra/orm/mongoose/repository/companyRepository.ts
+++ backend/src/infra/orm/mongoose/repository/companyRepository.ts
@@ -1,18 +1,23 @@
 import { CompanyRepository } from '@application/protocols/db';
+import { Company } from '@domain/entities';
 import { CompanySignUpParams } from '@domain/useCases';
-import Company from '../schemas/Company';
+import CompanySchema from '../schemas/Company';
 
 export class CompanyRepositoryMongo implements CompanyRepository {
   async create(data: CompanySignUpParams): Promise<void> {
-    const company = new Company(data);
+    const company = new CompanySchema(data);
 
     await company.save();
   }
 
   async findByOwnerCpf(cpf: number): Promise<boolean> {
-    const company = await Company.findOne({ owner_company_cpf: cpf });
+    const company = await CompanySchema.findOne({ owner_company_cpf: cpf });
 
     if (company) return true;
     return false;
+  }
+
+  async getAll(): Promise<Company[]> {
+    return await CompanySchema.find()
   }
 }
