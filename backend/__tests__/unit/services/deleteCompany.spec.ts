d6c17cab9c5905f87b4c7f86b975b006eea9f915
Ederson S <edersonrodrigo31@gmail.com>
2021-06-13 10:01:09 -0300

    [TEST]: ensure delete company service returns an error if company does not exist

--- backend/__tests__/unit/services/deleteCompany.spec.ts
+++ backend/__tests__/unit/services/deleteCompany.spec.ts
@@ -1,4 +1,5 @@
 import { DeleteCompanyService } from '@application/services/deleteCompany';
+import { BaseError } from '@domain/errors';
 import { makeCompanyRepository } from '../moks/makeCompanyRepo';
 
 const makeSut = () => {
@@ -15,5 +16,18 @@ describe('Delete company', () => {
 
     await service.execute(companyId);
     expect(spyRepo).toHaveBeenCalledWith(companyId);
+  });
+  it('Should returns Error if company does not exist', async () => {
+    const { service, companyRepository } = makeSut();
+    jest.spyOn(companyRepository, 'findById').mockImplementation(() => new Promise((resolve) => resolve(false)));
+    const companyId = '123';
+
+    try {
+      await service.execute(companyId);
+    } catch (error) {
+      expect(error instanceof BaseError).toBe(true);
+      expect(error.message).toBe('Company does not exist');
+      expect(error.statusCode).toBe(400);
+    }
   });
 });
