f083be49f0976f0bd602ef737237ea2f42d06240
Ederson S <edersonrodrigo31@gmail.com>
2021-06-13 09:17:20 -0300

    [TEST]: ensure delete company controller calls service with correct data

--- backend/__tests__/unit/controllers/deleteCompany.spec.ts
+++ backend/__tests__/unit/controllers/deleteCompany.spec.ts
@@ -1,15 +1,34 @@
 import { DeleteCompanyController } from '@presentation/controllers/deleteCompany';
+import { DeleteCompanyStub } from '../moks/makeService';
 
 const makeSut = () => {
-  const controller = new DeleteCompanyController();
-  return controller;
+  const service = new DeleteCompanyStub();
+  const sut = new DeleteCompanyController(service);
+  return { service, sut };
 };
 
 describe('Delete Company', () => {
   it('Should return 204 when delete company', async () => {
-    const { handle } = makeSut();
+    const { sut } = makeSut();
+    const request = {
+      params: {
+        companyId: '3333',
+      },
+    };
 
-    const response = await handle();
+    const response = await sut.handle(request.params.companyId);
     expect(response.statusCode).toBe(204);
+  });
+  it('Should calls service with correct params', async () => {
+    const { sut, service } = makeSut();
+    const spyService = jest.spyOn(service, 'execute');
+    const request = {
+      params: {
+        companyId: '3333',
+      },
+    };
+
+    await sut.handle(request.params.companyId);
+    expect(spyService).toHaveBeenCalledWith('3333');
   });
 });
