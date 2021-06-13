d868db6139425bbc32fb529ce669a1a788ddd545
Ederson S <edersonrodrigo31@gmail.com>
2021-06-13 10:19:03 -0300

    [FEAT]: ensure delete company controller returns error if service throws

--- backend/src/presentation/controllers/deleteCompany.ts
+++ backend/src/presentation/controllers/deleteCompany.ts
@@ -1,12 +1,16 @@
 import { DeleteCompany } from '@domain/useCases/deleteCompay';
-import { noContent } from '@presentation/helpers/httpHelper';
+import { errorTreatment, noContent } from '@presentation/helpers/httpHelper';
 import { Controller, HttpResponse } from '@presentation/protocols';
 
 export class DeleteCompanyController implements Controller {
   constructor(private readonly deleteCompany: DeleteCompany) {}
 
   async handle(companyId: string): Promise<HttpResponse> {
-    await this.deleteCompany.execute(companyId);
+    try {
+      await this.deleteCompany.execute(companyId);
+    } catch (error) {
+      return errorTreatment(error);
+    }
     return noContent();
   }
 }
